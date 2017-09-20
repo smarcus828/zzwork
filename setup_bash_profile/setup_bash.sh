1a84e833d15de1a7a222ef5eb90185ed772370f2
Steven.Marcus <steven.marcus@noaa.gov>
2017-09-20 08:45:09 -0400

    added note

--- setup_bash_profile/setup_bash.sh
+++ setup_bash_profile/setup_bash.sh
@@ -9,9 +9,15 @@ if [[ $# -eq 0 ]]; then
   echo "Yeah - this script needs an argument.  A hostname or IP or something."
   sleep 3
   echo "dumbass"
-  exit 0
+  exit 0 
 fi
 
 scp ${LHOMEDIR}${SCRIPT} ${1}: &>/dev/null
 ssh ${1} ${RHOMEDIR}${SCRIPT} &>/dev/null
 
+#create a function at the command line - because an alias wont take an argument
+#sshh() {
+#> cd /home/steven.marcus/zzwork/setup_bash_profile/
+#> ./setup_bash.sh $1
+#> }
+#
